405ed00daa7626eb972400701e83c35dbf07aa2d
chandrakala2424 <chandrakala.p@supportmail.co.in>
2026-06-05 16:49:15 +0530

    pushing

---
+++ LoginMVC/Controllers/AccountController.cs
@@ -0,0 +1,60 @@
+﻿using LoginMVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LoginMVC.Controllers
+{
+    public class AccountController : Controller
+    {
+        // GET: Account
+        //public ActionResult Index()
+        //{
+        //    return View();
+        //}
+        public string cs = ConfigurationManager.ConnectionStrings["mConnection"].ToString();
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        // POST: Login
+        [HttpPost]
+        public ActionResult Login(UserModel model)
+        {
+            using (SqlConnection con = new SqlConnection(cs))
+            {
+                string query = "SELECT * FROM LOGIN_DETAILS WHERE Username=@Username AND Password=@Password";
+
+                SqlCommand cmd = new SqlCommand(query, con);
+                cmd.Parameters.AddWithValue("@Username", model.Username);
+                cmd.Parameters.AddWithValue("@Password", model.Password);
+
+                con.Open();
+
+                SqlDataReader dr = cmd.ExecuteReader();
+
+                if (dr.Read())
+                {
+                    //Session["UserId"] = dr["Id"].ToString();
+                    Session["Username"] = dr["Username"].ToString();
+
+                    return RedirectToAction("Index", "Home");
+                }
+            }
+
+            ViewBag.Error = "Invalid username or password";
+            return View();
+        }
+
+        public ActionResult Logout()
+        {
+            Session.Clear();
+            return RedirectToAction("Login");
+        }
+    }
+}
